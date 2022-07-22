4ef38f911b6155b849e40f6d9d5080bc6f2c25c5
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-22 10:52:10 +0300

    Arguments and parameters

--- app.js
+++ app.js
@@ -13,13 +13,15 @@ function greet(){
 //function expression, we give the function a name because
 //its name is now the variable name or in this case the const name
 //function expressions are not hoistec
-const speak = function(){
-    console.log("Good day!")
-}
+const speak = function(name, time){
+    console.log(`Good ${time} ${name}`);
+};
 
 //calling/invoking the function
 // greet();
 // greet();
-speak();
+speak("Mario", 'morning');
+
+
 
 
