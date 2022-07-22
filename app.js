6e6f91a8dbd213668b1b6955c6a92d0c48927e71
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-22 10:29:43 +0300

    hoisting of functions

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+greet();
+greet();
+
+//speak(); wont work because javascript doesn't hoist function expressions
+
+
 //function declaration
+//function declaration are hoisted
 function greet(){
     console.log("Hello there");
 }
 
 //function expression, we give the function a name because
 //its name is now the variable name or in this case the const name
+//function expressions are not hoistec
 const speak = function(){
     console.log("Good day!")
 }
 
 //calling/invoking the function
-greet();
-greet();
-
+// greet();
+// greet();
 speak();
 
 
