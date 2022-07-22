d613930744514e45f5449590642c5a852fdaae8b
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-22 11:34:55 +0300

    Arrow functions

--- app.js
+++ app.js
@@ -32,6 +32,29 @@ const calcArea = function(radius){
 const area = calcArea(5);
 console.log(area);
 
+//Arrow functions
+//Your can remove the parantheses if there is only one parameter
+// const round = radius => {
+//     return 3.14 * radius**2
+// };
 
+//if there is only one return statement, you can remove the return
+//keyworld and the block curly braces likes so
+const round = radius => 3.14 * radius**2
 
+const value = round(5);
+console.log(value);
 
+const greet = () => 'hello, world';
+const greetings = greet();
+console.log(greetings);
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+
+console.log(bill([10,15,30], 0.2));
