2a110c4133af5816ba7fe1841bf3f48588252b16
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-22 11:01:24 +0300

    Returning values from a function

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-greet();
-greet();
+// greet();
+// greet();
 
-//speak(); wont work because javascript doesn't hoist function expressions
+// //speak(); wont work because javascript doesn't hoist function expressions
 
 
-//function declaration
-//function declaration are hoisted
-function greet(){
-    console.log("Hello there");
-}
+// //function declaration
+// //function declaration are hoisted
+// function greet(){
+//     console.log("Hello there");
+// }
 
-//function expression, we give the function a name because
-//its name is now the variable name or in this case the const name
-//function expressions are not hoistec
-//asign some default value to th function
-const speak = function(name = 'Luigi', time = 'night'){
-    console.log(`Good ${time} ${name}`);
+// //function expression, we give the function a name because
+// //its name is now the variable name or in this case the const name
+// //function expressions are not hoistec
+// //asign some default value to th function
+// const speak = function(name = 'Luigi', time = 'night'){
+//     console.log(`Good ${time} ${name}`);
+// };
+
+// //calling/invoking the function
+// // greet();
+// // greet();
+// speak();
+
+
+//Returning values
+const calcArea = function(radius){
+    let area = 3.14 * radius**2;
+    return area;
 };
 
-//calling/invoking the function
-// greet();
-// greet();
-speak();
+const area = calcArea(5);
+console.log(area);
 
 
 
