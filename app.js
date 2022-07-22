b4b83ccb4b31c934c6f0209a5a934f9b5e7ccfe0
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-22 10:18:35 +0300

    function declaration, func expression

---
+++ app.js
@@ -0,0 +1,18 @@
+//function declaration
+function greet(){
+    console.log("Hello there");
+}
+
+//function expression, we give the function a name because
+//its name is now the variable name or in this case the const name
+const speak = function(){
+    console.log("Good day!")
+}
+
+//calling/invoking the function
+greet();
+greet();
+
+speak();
+
+
